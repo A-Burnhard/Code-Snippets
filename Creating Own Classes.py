162d9328cf7e03c8487a7dc9214fc63d776d35e4
Appiedu Bernard <benadkojo@gmail.com>
2023-04-20 13:55:52 +0000

    initial

--- Creating Own Classes.py
+++ Creating Own Classes.py
@@ -1,9 +1,9 @@
-class Names:
+class Title:
     def __init__(self,name,sex):
         self.name = name
         self.sex = sex
 
-    def annotate(self):
+    def assign(self):
         if self.sex == 'male':
             return f"Mr. {self.name}"
         else:
@@ -12,5 +12,5 @@ class Names:
 name = input("Enter your name: ")
 sex = input("Are you a male or female? ").lower() # Convert the input to lower case
 
-e = Names(name,sex)
-print(e.annotate()) # Pass the 'sex' argument when calling annotate() method
+e = Title(name,sex)
+print(e.assign()) # Pass the 'sex' argument when calling annotate() method
